fa6331c5264a86f4e1c362a21238acb9863bd481
akul <nurislamimanudin@gmail.com>
2024-03-14 00:37:27 +0700

    dart package

---
+++ example/hello.dart
@@ -0,0 +1,13 @@
+import 'package:dart_package/hello.dart';
+
+void main() {
+  var hello = sayHello('Akul');
+  print(hello);
+  sample();
+
+  var customer = Customer();
+  var category = Category();
+
+  print(customer);
+  print(category);
+}
---
+++ lib/hello.dart
@@ -0,0 +1,4 @@
+library dart_package;
+
+export 'src/say_hello.dart';
+export 'src/customer.dart' show Customer, Category;
